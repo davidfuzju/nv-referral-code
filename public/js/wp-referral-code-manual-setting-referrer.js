97db38ec148098b51c0061aa0d32f0fe32e8bdfe
DavidFU <david.fu@harmay.com>
2024-10-27 17:25:30 +0800

    add wrc prefix for manual setting referral short code

--- public/js/wp-referral-code-manual-setting-referrer.js
+++ public/js/wp-referral-code-manual-setting-referrer.js
@@ -1,19 +1,19 @@
 jQuery(document).ready(function ($) {
   $("body").append(`
-	<div id="referral-popup-overlay">
-		<div id="referral-popup-content">
-    			<form id="referral-form">
-        			<div id="warning-message">
+	<div id="wrc-referral-popup-overlay">
+		<div id="wrc-referral-popup-content">
+    			<form id="wrc-referral-form">
+        			<div id="wrc-warning-message">
            				<p>WARNING<br>
             				Please be advised that once you commit, it cannot be changed.</p>
         			</div>
 
-        			<label id="referral-code-label" for="referral-code"></label>
-        			<input type="text" id="referral-code" name="referral-code" placeholder="Referrer Membership No." value="">
+        			<label id="wrc-referral-code-label" for="wrc-referral-code"></label>
+        			<input type="text" id="wrc-referral-code" name="wrc-referral-code" placeholder="Referrer Membership No." value="">
 
-        			<div id="referral-buttons">
-            				<button type="button" id="cancel-button">Cancel</button>
-            				<button type="submit" id="commit-button">Commit</button>
+        			<div id="wrc-referral-buttons">
+            				<button type="button" id="wrc-cancel-button">Cancel</button>
+            				<button type="submit" id="wrc-commit-button">Commit</button>
         			</div>
     			</form>
 		</div>
@@ -24,16 +24,16 @@ jQuery(document).ready(function ($) {
   $("#nv-referral-code-manual-setting-referrer-button").on(
     "click",
     function () {
-      $("#referral-popup-overlay").css("display", "flex"); // Show the overlay
+      $("#wrc-referral-popup-overlay").css("display", "flex"); // Show the overlay
       $("body").css("overflow", "hidden"); // Disable scrolling on the body
     }
   );
 
   // Handle the form submission
-  $("#referral-form").submit(function (e) {
+  $("#wrc-referral-form").submit(function (e) {
     e.preventDefault();
 
-    var referralCode = $("#referral-code").val();
+    var referralCode = $("#wrc-referral-code").val();
 
     // Prepare the data for the AJAX request
     var data = {
@@ -44,7 +44,7 @@ jQuery(document).ready(function ($) {
     // Send the AJAX request to the server
     $.post(ajaxurl, data, function (response) {
       if (response == 0) {
-        $("#referral-code-label").text(
+        $("#wrc-referral-code-label").text(
           "An error occurred: Invalid action specified."
         );
       } else if (response.success) {
@@ -54,9 +54,9 @@ jQuery(document).ready(function ($) {
             response.data.referrer_name
         );
 
-        $("#referral-popup-overlay").css("display", "none");
+        $("#wrc-referral-popup-overlay").css("display", "none");
         $("body").css("overflow", "auto");
-        $("#referral-code-label").text("");
+        $("#wrc-referral-code-label").text("");
 
         // Page reload after successful submission
         setTimeout(function () {
@@ -66,38 +66,40 @@ jQuery(document).ready(function ($) {
       } else {
         switch (response.data.error_code) {
           case "REFERRAL_CODE_MISSING":
-            $("#referral-code-label").text("Please enter a referral code.");
+            $("#wrc-referral-code-label").text("Please enter a referral code.");
             break;
           case "REFERRAL_NOT_FOUND":
-            $("#referral-code-label").text(
+            $("#wrc-referral-code-label").text(
               "Invalid referral code. Please try again."
             );
             break;
           case "USER_NOT_LOGGED_IN":
-            $("#referral-code-label").text(
+            $("#wrc-referral-code-label").text(
               "You must be logged in to apply a referral code."
             );
             break;
           case "ALREADY_REFERRED":
-            $("#referral-code-label").text("You have already been referred.");
+            $("#wrc-referral-code-label").text(
+              "You have already been referred."
+            );
             break;
           default:
-            $("#referral-code-label").text(
+            $("#wrc-referral-code-label").text(
               "An error occurred: " + response.data.message
             );
         }
       }
     }).fail(function () {
-      $("#referral-code-label").text(
+      $("#wrc-referral-code-label").text(
         "An error occurred: " + response.data.message
       );
     });
   });
 
   // Handle the Cancel button click - Close the popup
-  $("#cancel-button").click(function () {
-    $("#referral-popup-overlay").css("display", "none");
+  $("#wrc-cancel-button").click(function () {
+    $("#wrc-referral-popup-overlay").css("display", "none");
     $("body").css("overflow", "auto");
-    $("#referral-code-label").text("");
+    $("#wrc-referral-code-label").text("");
   });
 });
